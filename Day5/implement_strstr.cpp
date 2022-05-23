4ebac000362112fd2d64a6634f59d41794a26a8f
Vaibhav Jaiswal <vaibhav.jaiswal.vj91@gmail.com>
2022-05-23 09:18:30 +0530

    Day_5_Completed

---
+++ Day5/implement_strstr.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+       
+        int m = haystack.size();
+        int n = needle.size();
+        if (n == 0) {
+            return 0;
+        }
+        
+        if (m == 0) {
+            return -1;
+        }
+        
+        int flag = 0;
+        for (int i = 0; i < m; i++) {
+            if (haystack[i] == needle[0]) {
+                flag = 0;
+                for (int j = 0; j < n; j++) {
+                    if (haystack[i+j] != needle[j]) {
+                        flag = 1;
+                        break;
+                    }
+                }
+                if (flag == 0) {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+    
+};
